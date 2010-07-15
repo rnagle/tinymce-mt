3de82cacb7b4875f16ac5e51358b7256538d328e
rnagle <rnagle@tribune.com>
2010-07-15 12:44:02 -0500

    trying another method of making the plugin thread safe for fcgi

--- plugins/TinyMCE/lib/TinyMCE/Plugin.pm
+++ plugins/TinyMCE/lib/TinyMCE/Plugin.pm
@@ -14,19 +14,24 @@ sub set_editor_prefs {
     my $plugin = plugin();
     my $scope  = "blog:" . $blog->id;
     my $use_tinymce = $plugin->get_config_value('use_tinymce',$scope);
-
+    
     # If 'Use TinyMCE' is enabled for this blog, enable it on entry creation page
     unless ( !defined $use_tinymce || $use_tinymce == " " ) {
-        my $tmpl_file = $plugin->path.'/tmpl/cms/include/archetype_editor.tmpl';
-#        $app->config( 'AltTemplatePath', $tmpl_file );
-        $param->{rich_editor} = '1';
-        
-        my $basename_field = $tmpl->getElementById('basename');
-        my $innerHTML = "<mt:include name=\"$tmpl_file\">";
-        my $editor_elem = $tmpl->createElement('setvarblock',{ name => 'rich_editor_tmpl' });
-        $editor_elem->innerHTML($innerHTML);
-        $tmpl->insertBefore($editor_elem, $basename_field);
+        my $tmpl_file = $plugin->path.'/tmpl';
+        $app->config( 'AltTemplatePath', $tmpl_file );
 
+        # my $tmpl_file = $plugin->path.'/tmpl/cms/include/archetype_editor.tmpl';
+        # $param->{rich_editor} = '1';
+        # 
+        # my $basename_field = $tmpl->getElementById('basename');
+        # my $innerHTML = "<mt:include name=\"$tmpl_file\">";
+        # my $editor_elem = $tmpl->createElement('setvarblock',{ name => 'rich_editor_tmpl' });
+        # $editor_elem->innerHTML($innerHTML);
+        # $tmpl->insertBefore($editor_elem, $basename_field);
+    } else {
+        # Make sure the default template path is set -- this should help avoid problems when using FastCGI
+        my $tmpl_file = $app->path.'tmpl';
+        $app->config('AltTemplatePath', $tmpl_file);
     }
 }
 
