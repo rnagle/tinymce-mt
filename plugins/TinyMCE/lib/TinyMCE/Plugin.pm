10c41e6c851b4839aead95cc17358822b9ae06fc
rnagle <rnagle@tribune.com>
2010-07-15 12:55:30 -0500

    cleaning up old code and straightening out some comments

--- plugins/TinyMCE/lib/TinyMCE/Plugin.pm
+++ plugins/TinyMCE/lib/TinyMCE/Plugin.pm
@@ -15,21 +15,12 @@ sub set_editor_prefs {
     my $scope  = "blog:" . $blog->id;
     my $use_tinymce = $plugin->get_config_value('use_tinymce',$scope);
     
-    # If 'Use TinyMCE' is enabled for this blog, enable it on entry creation page
     unless ( !defined $use_tinymce || $use_tinymce == " " ) {
+    # If 'Use TinyMCE' is enabled for this blog, override the default text edtior template file location
         my $tmpl_file = $plugin->path.'/tmpl';
         $app->config( 'AltTemplatePath', $tmpl_file );
-
-        # my $tmpl_file = $plugin->path.'/tmpl/cms/include/archetype_editor.tmpl';
-        # $param->{rich_editor} = '1';
-        # 
-        # my $basename_field = $tmpl->getElementById('basename');
-        # my $innerHTML = "<mt:include name=\"$tmpl_file\">";
-        # my $editor_elem = $tmpl->createElement('setvarblock',{ name => 'rich_editor_tmpl' });
-        # $editor_elem->innerHTML($innerHTML);
-        # $tmpl->insertBefore($editor_elem, $basename_field);
     } else {
-        # Make sure the default template path is set -- this should help avoid problems when using FastCGI
+    # Otherwise, make sure the default template path is set -- this should help avoid problems when using FastCGI
         my $tmpl_file = $app->path.'tmpl';
         $app->config('AltTemplatePath', $tmpl_file);
     }
