414596400b7c88435fdc8f17fbb542635f9147c7
rnagle <rnagle@tribune.com>
2010-10-12 18:04:04 -0500

    refactoring the plugin, adding rich text editor selection menu to the preferences pane

--- plugins/TinyMCE/lib/TinyMCE/Plugin.pm
+++ plugins/TinyMCE/lib/TinyMCE/Plugin.pm
@@ -3,6 +3,7 @@ package TinyMCE::Plugin;
 use strict;
 use MT::App;
 use MT::Template;
+use Switch;
 
 sub plugin {
     return MT->component('TinyMCE');
@@ -13,17 +14,80 @@ sub set_editor_prefs {
     my $blog = MT->instance->blog;
     my $plugin = plugin();
     my $scope  = "blog:" . $blog->id;
-    my $use_tinymce = $plugin->get_config_value('use_tinymce',$scope);
     
-    unless ( !defined $use_tinymce || $use_tinymce == " " ) {
-    # If 'Use TinyMCE' is enabled for this blog, override the default text edtior template file location
-        my $tmpl_file = $plugin->path.'/tmpl';
-        $app->config( 'AltTemplatePath', $tmpl_file );
-    } else {
-    # Otherwise, make sure the default template path is set -- this should help avoid problems when using FastCGI
-        my $tmpl_file = $app->path.'tmpl';
-        $app->config('AltTemplatePath', $tmpl_file);
+    my $use_tinymce = ( !defined($plugin->get_config_value('tinymce_on',$scope)) ? 'off' : 'on' );
+    
+   switch ($use_tinymce) {
+       case 'on' { 
+           my $tmpl_file = $plugin->path.'/tmpl';
+           $app->config( 'AltTemplatePath', $tmpl_file );
+           } else {
+           my $tmpl_file = $app->path.'tmpl';
+           $app->config('AltTemplatePath', $tmpl_file);
+       }
     }
+}
+
+sub apply {
+    my $app     = shift;
+    my ($param) = @_;
+
+    $app->validate_magic or return;
+
+    my $q       = $app->{query};
+    my $blog    = MT->model('blog')->load( $q->param('blog_id') );
+    my $pval     = $q->param('pref_value');
+
+    my $plugin = plugin();
+    my $scope  = "blog:" . $blog->id;
+    $plugin->set_config_value('tinymce_on',$pval,$scope);
+
+    return $app->redirect(
+    $app->uri(
+        'mode' => 'manage_editor',
+        'args' => {
+        'prefs_applied' => 1,
+        'blog_id'       => $app->param('blog_id'),
+        'return_args'   => $app->return_args,
+        'magic_token'   => $app->param('magic_token')
+        }
+    )
+    );
+}
+
+sub manage_editor {
+    my $app     = shift;
+    my ($param) = @_;
+    my $q       = $app->{query};
+    my $blog    = MT->model('blog')->load( $q->param('blog_id') );
+
+    $param ||= {};
+
+    my $prefs = MT->registry('rte_prefs');
+    
+    my $plugin = plugin();
+    my $scope  = "blog:" . $blog->id;
+    my $use_tinymce = $plugin->get_config_value('tinymce_on',$scope);
+    
+    my @data;
+    foreach my $pval (keys %$prefs) {
+        my $pref = $prefs->{$pval};
+        push @data, {
+            id => $pval,
+            name => &{ $pref->{'label'} },
+            description => $pref->{'description'},
+            order => $pref->{'order'} || 10,
+            value => $use_tinymce
+            };
+    }
+    @data = sort { $a->{order} <=> $b->{order} } @data;
+    $param->{prefs} = \@data;
+    $param->{blog_id} = $app->param('blog_id');
+    $param->{return_args} = $app->return_args;
+    $param->{magic_token} = $app->param('magic_token');
+    $param->{prefs_applied} = $app->param('prefs_applied') ? 1 : 0;
+
+    return $app->load_tmpl( 'manage_editor.tmpl', $param );
 }
 
 1;
